1a69f5fcf1cc929f1d0c29a69c33c2a8a3b60fd3
Mauro Goya <mauro.goya@radiumrocket.com>
2021-04-13 11:54:49 -0300

    Add type submit at the final button of the wizard

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -11,39 +11,26 @@ const Controls = ({
   nextStep,
   previousStep,
   totalSteps,
-}: any) => (
-  <div>
-    <hr />
-    {currentStep > 1 && (
-      <button className="btn btn-default btn-block" onClick={previousStep}>
-        Go Back
-      </button>
-    )}
-    {currentStep < totalSteps ? (
-      <button className="btn btn-primary btn-block" onClick={nextStep}>
-        Continue
-      </button>
-    ) : (
-      <button className="btn btn-success btn-block" onClick={nextStep}>
-        Finish
-      </button>
-    )}
-    <hr />
-    <div style={{ fontSize: "21px" }}>
-      <h4>Other Functions</h4>
-      <div>Current Step: {currentStep}</div>
-      <div>Total Steps: {totalSteps}</div>
-      <button className="btn btn-block btn-default" onClick={firstStep}>
-        First Step
-      </button>
-      <button className="btn btn-block btn-default" onClick={lastStep}>
-        Last Step
-      </button>
-      <button className="btn btn-block btn-default" onClick={() => goToStep(2)}>
-        Go to Step 2
-      </button>
+}: any) => {
+  return (
+    <div>
+      <hr />
+      {currentStep > 1 && <button onClick={previousStep}>Go Back</button>}
+      {currentStep < totalSteps ? (
+        <button onClick={nextStep}>Continue</button>
+      ) : (
+        <button type="submit">Finish</button>
+      )}
+      <hr />
+      <div style={{ fontSize: "21px" }}>
+        <h4>Other Functions</h4>
+        <div>Current Step: {currentStep}</div>
+        <div>Total Steps: {totalSteps}</div>
+        <button onClick={firstStep}>First Step</button>
+        <button onClick={lastStep}>Last Step</button>
+        <button onClick={() => goToStep(2)}>Go to Step 2</button>
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Controls;
