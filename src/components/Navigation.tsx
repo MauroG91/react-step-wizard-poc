3ec1069448844215aa40beb75a161db29847b170
Mauro Goya <mauro.goya@radiumrocket.com>
2021-04-13 11:54:16 -0300

    Add navigation dots and funcionality

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from "./Navigation.module.scss";
 
 interface Props {}
 
-const Navigation = (props: Props) => {
-  return <div>Nav</div>;
+const Navigation = (props: any) => {
+  const { totalSteps, currentStep, goToStep } = props;
+  const dots = [];
+  for (let i = 1; i <= totalSteps; i += 1) {
+    const isActive = currentStep === i;
+    dots.push(
+      <span
+        key={`step-${i}`}
+        className={`${styles.dot} ${isActive ? styles.active : ""}`}
+        onClick={() => goToStep(i)}
+      >
+        &bull;
+      </span>
+    );
+  }
+  return <div className={styles.nav}>{dots}</div>;
 };
 
 export default Navigation;
