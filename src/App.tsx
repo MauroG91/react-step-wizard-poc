7947edb828e8e4a5c89b8c523c7a3292bb563f76
Mauro Goya <mauro.goya@radiumrocket.com>
2021-04-14 15:16:10 -0300

    Make the wizard reusable

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
 import { FormProvider, useForm } from "react-hook-form";
-import FormWrapper from "./FormWrapper";
+import Wizard from "./Wizard";
 import { Form } from "semantic-ui-react";
+import Step1 from "./components/Step1";
+import Step2 from "./components/Step2";
+import Step3 from "./components/Step3";
+
+const steps = [
+  {
+    name: "step1",
+    component: Step1,
+  },
+  {
+    name: "step2",
+    component: Step2,
+  },
+  {
+    name: "step3",
+    component: Step3,
+  },
+];
 
 function App() {
   const methods = useForm({
@@ -13,7 +31,7 @@ function App() {
   return (
     <FormProvider {...methods}>
       <Form onSubmit={methods.handleSubmit(onSubmit)}>
-        <FormWrapper />
+        <Wizard steps={steps} navigation={true} />
       </Form>
     </FormProvider>
   );
--- src/Wizard.tsx
+++ src/Wizard.tsx
@@ -2,42 +2,27 @@ import React from "react";
 import StepWizard, { StepWizardChildProps } from "react-step-wizard";
 import { Grid } from "semantic-ui-react";
 import Navigation from "./components/Navigation";
-import Step1 from "./components/Step1";
-import Step2 from "./components/Step2";
-import Step3 from "./components/Step3";
 
-const components = [
-  {
-    name: "step1",
-    component: Step1,
-  },
-  {
-    name: "step2",
-    component: Step2,
-  },
-  {
-    name: "step3",
-    component: Step3,
-  },
-];
+interface Props {
+  steps: Child[];
+  navigation?: boolean;
+}
 
 type Child = {
   name: string;
   component: React.FC<StepWizardChildProps>;
 };
 
-const FormWrapper = () => {
+const Wizard = ({ steps, navigation = false }: Props) => {
   const handleRender = () => {
-    return components.map((child: Child) =>
-      React.createElement(child.component)
-    );
+    return steps.map((child: Child) => React.createElement(child.component));
   };
 
   return (
     <Grid>
       <StepWizard
         initialStep={1}
-        nav={<Navigation />}
+        nav={navigation ? <Navigation /> : <div />}
         isHashEnabled={false}
         isLazyMount={true}
       >
@@ -47,4 +32,4 @@ const FormWrapper = () => {
   );
 };
 
-export default FormWrapper;
+export default Wizard;
